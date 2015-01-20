e276c9e3b8ff1fa136ee469f8d12dd4c0df901ed
Jeffro Ligon <jtligon@gmail.com>
2015-01-20 12:55:08 -0500

    clang formatted viewController

--- MealRider/ViewController.m
+++ MealRider/ViewController.m
@@ -8,7 +8,6 @@
 
 #import "ViewController.h"
 
-
 @interface ViewController ()
 
 @end
@@ -16,81 +15,92 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+  [super viewDidLoad];
 
-    // Do any additional setup after loading the view, typically from a nib.
-    if (!self.rc) {
-        self.rc = [[RequestCreator alloc]init];
-    }
-    self.locationManager = [[CLLocationManager alloc]init];
-    self.geocoder = [[CLGeocoder alloc]init];
-
+  // Do any additional setup after loading the view, typically from a nib.
+  if (!self.rc) {
+    self.rc = [[RequestCreator alloc] init];
+  }
+  self.locationManager = [[CLLocationManager alloc] init];
+  self.geocoder = [[CLGeocoder alloc] init];
 }
 
--(IBAction)sendRequest:(id)sender{
-    NSLog(@"Button Pressed - VC");
-    
-//    [self.rc sendRequestToTransloc];
-    [self.rc genericRequestTranslocWithEndpoint:@"routes" geoArea:@"35.80176%2C-78.64347%7C35.78061%2C-78.68218"];
-}
+- (IBAction)sendRequest:(id)sender {
+  NSLog(@"Button Pressed - VC");
 
+  //    [self.rc sendRequestToTransloc];
+  [self.rc
+      genericRequestTranslocWithEndpoint:
+          @"routes" geoArea:@"35.80176%2C-78.64347%7C35.78061%2C-78.68218"];
+}
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    NSLog(@"memory!");
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  NSLog(@"memory!");
+  // Dispose of any resources that can be recreated.
 }
-
 
 #pragma mark Location Stuff
 
-- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
-{
-    NSLog(@"didFailWithError: %@", error);
-    UIAlertView *errorAlert = [[UIAlertView alloc]
-                               initWithTitle:@"Error" message:@"Failed to Get Your Location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-    [errorAlert show];
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error {
+  NSLog(@"didFailWithError: %@", error);
+  UIAlertView *errorAlert =
+      [[UIAlertView alloc] initWithTitle:@"Error"
+                                 message:@"Failed to Get Your Location"
+                                delegate:nil
+                       cancelButtonTitle:@"OK"
+                       otherButtonTitles:nil];
+  [errorAlert show];
 }
 
-- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
-{
-    NSLog(@"didUpdateToLocation: %@", newLocation);
-    CLLocation *currentLocation = newLocation;
-    
-    if (currentLocation != nil) {
-        self.longitudeLabel.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
-        self.latitudeLabel.text = [NSString stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
-    }
-    
-    // Reverse Geocoding
-    NSLog(@"Resolving the Address");
-    [self.geocoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray *placemarks, NSError *error) {
-        NSLog(@"Found placemarks: %@, error: %@", placemarks, error);
-        if (error == nil && [placemarks count] > 0) {
-            self.placemark = [placemarks lastObject];
-            self.addressLabel.text = [NSString stringWithFormat:@"%@ %@\n%@ %@\n%@\n%@",
-                                 self.placemark.subThoroughfare, self.placemark.thoroughfare,
-                                 self.placemark.postalCode, self.placemark.locality,
-                                 self.placemark.administrativeArea,
-                                 self.placemark.country];
-        } else {
-            NSLog(@"%@", error.debugDescription);
-        }
-    } ];
-    [self.locationManager stopUpdatingLocation];
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation {
+  NSLog(@"didUpdateToLocation: %@", newLocation);
+  CLLocation *currentLocation = newLocation;
+
+  if (currentLocation != nil) {
+    self.longitudeLabel.text = [NSString
+        stringWithFormat:@"%.8f", currentLocation.coordinate.longitude];
+    self.latitudeLabel.text = [NSString
+        stringWithFormat:@"%.8f", currentLocation.coordinate.latitude];
+  }
+
+  // Reverse Geocoding
+  NSLog(@"Resolving the Address");
+  [self.geocoder
+      reverseGeocodeLocation:currentLocation
+           completionHandler:^(NSArray *placemarks, NSError *error) {
+             NSLog(@"Found placemarks: %@, error: %@", placemarks, error);
+             if (error == nil && [placemarks count] > 0) {
+               self.placemark = [placemarks lastObject];
+               self.addressLabel.text =
+                   [NSString stringWithFormat:@"%@ %@\n%@ %@\n%@\n%@",
+                                              self.placemark.subThoroughfare,
+                                              self.placemark.thoroughfare,
+                                              self.placemark.postalCode,
+                                              self.placemark.locality,
+                                              self.placemark.administrativeArea,
+                                              self.placemark.country];
+             } else {
+               NSLog(@"%@", error.debugDescription);
+             }
+           }];
+  [self.locationManager stopUpdatingLocation];
 }
 
--(void)locationAquired{
-    //we've got a lock, now we can request a location
+- (void)locationAquired {
+  // we've got a lock, now we can request a location
 }
 
 - (IBAction)getCurrLocation:(id)sender {
-    
-    self.locationManager.delegate = self;
-    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    if ([CLLocationManager locationServicesEnabled]) {
-        [self.locationManager startUpdatingLocation];
-    }
+
+  self.locationManager.delegate = self;
+  self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+  if ([CLLocationManager locationServicesEnabled]) {
+    [self.locationManager startUpdatingLocation];
+  }
 }
 
 @end
